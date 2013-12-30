c43e16a1ae7077860f06303030a7d2004d058127
Mike Wyatt <wyatt.mike@gmail.com>
2013-12-30 05:44:05 -0400

    remove old examples folder

--- examples/blog_example/tests/test_blog.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-
-from tests import BaseTestCase as TestCase
-from flask import url_for
-
-class TestBlogViews(TestCase):
-    def test_index_view(self):
-        url_path = url_for('blog.views.index_view')
-        self.client.get(url_path)
