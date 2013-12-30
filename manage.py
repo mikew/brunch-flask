c0c5978e5f5611d5ecd3759cf5100e13ec67a901
Mike Wyatt <wyatt.mike@gmail.com>
2013-12-30 14:07:47 -0400

    routes management command

--- manage.py
+++ manage.py
@@ -22,5 +22,28 @@ def clear_cache():
     cache.clear()
 
 
+@manager.command
+def routes():
+    from server.main import app
+    from flask import url_for
+    import urllib
+
+    output = []
+    for rule in app.url_map.iter_rules():
+
+        options = {}
+        for arg in rule.arguments:
+            options[arg] = "[{0}]".format(arg)
+
+        methods = ','.join(rule.methods)
+        url = url_for(rule.endpoint, **options)
+        line = urllib.unquote("{:50s} {:20s} {}"
+                              .format(rule.endpoint, methods, url))
+        output.append(line)
+
+    for line in sorted(output):
+        print line
+
+
 if __name__ == "__main__":
     manager.run()
