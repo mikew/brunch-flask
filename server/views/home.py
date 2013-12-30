0da0d0a9ab56e17d2be7b5fbac979cd1837dcccf
Mike Wyatt <wyatt.mike@gmail.com>
2013-12-30 05:24:52 -0400

    index.html for server

--- server/views/home.py
+++ server/views/home.py
@@ -1,27 +1,9 @@
 from server.main import app, cache
-from server.util import Duration, json_response, template_or_json
-
-
-class Struct(object):
-    def __init__(self, *args, **kwargs):
-        for k, v in kwargs.iteritems():
-            setattr(self, k, v)
-
-    @property
-    def serialized(self):
-        return vars(self)
+from server.util import Duration, templated
 
 
 @app.route('/')
 @cache.cached(Duration.FOREVER)
-@json_response
-def hello():
-    return ['hello world']
-
-
-@app.route('/template')
-@cache.cached(Duration.FOREVER)
-@template_or_json()
-def hello2():
-    foo = dict(foo='bar')
-    return foo
+@templated('index.html')
+def index():
+    return {}
