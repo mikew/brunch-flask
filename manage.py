b47858d6ea3e2bbfdc84802756dd12c48b633bf9
Mike Wyatt <wyatt.mike@gmail.com>
2013-12-30 04:36:01 -0400

    default server to 0.0.0.0 in manage.py

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
-from flask_script import Manager
+from flask_script import Server, Manager
 
 from server import app_factory, cache, db, models
 manager = Manager(app_factory)
+
+manager.add_command("runserver", Server(host='0.0.0.0'))
 
 from server.managers.db import manager as db_manager
 manager.add_command('db', db_manager)
